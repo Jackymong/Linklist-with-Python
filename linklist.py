43d2deafc72819642075f5726f0a9f29364057ed
Jacky Mong Marma <93147721+Jackymong@users.noreply.github.com>
2022-08-26 11:39:58 +0600

    Update linklist.py

--- linklist.py
+++ linklist.py
@@ -125,3 +125,67 @@ if __name__ == "__main__":
     List.insertAtBetween(node2, 45)
     List.insertAtEnd(6)
     List.printList()
+
+    #Search from list
+    #Link insert between
+class Node(object):
+    def __init__(self,data,next=None):
+        self.data = data
+        self.next = next
+    def setNext(self,next):
+        self.next = next
+        
+class LinkList(object):
+    def __init__(self):
+        self.head= None
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(f'{temp.data}->',end=" ")
+            temp = temp.next
+        print("NULL")
+
+    def searchList(self, key):
+        temp = self.head
+        while(temp):
+            if temp.data == key:
+                print('Key Found')
+                return
+            temp = temp.next
+        print('Key not found')
+        
+    def insertAtStart(self,data):
+        if self.head == None:
+            newNode = Node(data)
+            self.head = newNode
+        else:
+            newNode = Node(data)
+            newNode.next = self.head
+            self.head = newNode
+            
+    def insertAtEnd(self,data):
+        newNode = Node(data)
+        temp = self.head
+        while(temp.next != None):
+            temp = temp.next
+        temp.next = newNode
+        
+    def insertAtBetween(self, previousNode, data):
+        if previousNode.next is None:
+            print("Previous node con't be none")
+        else:
+            newNode = Node(data)
+            newNode.next = previousNode.next
+            previousNode.next = newNode
+        
+if __name__ == "__main__":
+    List = LinkList()
+    List.head = Node(10)
+    node2 = Node(2)
+    List.head.setNext(node2)
+    node3 = Node(35)
+    node2.setNext(node3)
+    List.insertAtBetween(node2, 45)
+    List.insertAtEnd(6)
+    List.printList()
+    List.searchList(10)
